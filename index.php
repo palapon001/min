7df4fdc58c6fe7ffd32b679d62ed5777bef7cb14
Palapon Thitithanaporn <palapon.thit@kkpfg.com>
2023-01-16 09:21:01 +0700

    fix

--- index.php
+++ index.php
@@ -15,7 +15,6 @@
     <div class="h-100 d-flex align-items-center justify-content-center">
         <div class="card" style="width: 25rem;">
             <div class="card-body">
-                <center>
                     <form name="form1" method="post" action="check_login.php">
                         <p>
                         <h1>SCQA</h1>
@@ -35,7 +34,6 @@
                         </div>
                         <p><input type="submit" name="Submit" value="เข้าสู่ระบบ" class="btn btn-primary"></p>
                     </form>
-                </center>
             </div>
         </div>
     </div>
