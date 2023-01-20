973083d6f3beb7edf4d8faaf67a131240c349fa1
Palapon Thitithanaporn <palapon.thit@kkpfg.com>
2023-01-20 15:27:34 +0700

    fix

--- condb.php
+++ condb.php
@@ -1,10 +1,10 @@
 <?php
-// $con= mysqli_connect("h1use0ulyws4lqr1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","if3ryonp8v2yhks5","e3wg4fsqc8vljrh0","mdbdl32mu2ovolnh");
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "min";
-$con= mysqli_connect($host,$user,$pass,$db);//Local
+$con= mysqli_connect("h1use0ulyws4lqr1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","if3ryonp8v2yhks5","e3wg4fsqc8vljrh0","mdbdl32mu2ovolnh");
+// $host = "localhost";
+// $user = "root";
+// $pass = "";
+// $db = "min";
+// $con= mysqli_connect($host,$user,$pass,$db);//Local
 if(!$con){
    die("condb err");
 }
